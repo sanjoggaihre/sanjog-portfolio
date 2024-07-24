b166c2db8187ec19fc5b36a9eeb141db4043b1d6
Sanjog Gaihre <sanjoggaihre0@gmail.com>
2024-07-24 15:08:43 +0545

    commited

--- src/pages/Publications/Publications.page.tsx
+++ src/pages/Publications/Publications.page.tsx
@@ -28,7 +28,7 @@ export function PublicationsPage() {
   const [response, setResponse] = React.useState<any>(null);
 
   React.useEffect(() => {
-    fetch("/public/publications.json")
+    fetch("/sanjog-portfolio/public/publications.json")
       .then((res) => res.json())
       .then((res) => setResponse(res))
       .catch(() => setError(true))
