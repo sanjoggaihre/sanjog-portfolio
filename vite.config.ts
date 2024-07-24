5dda98afde867f4ef6baf06132db225974e58b28
Sanjog Gaihre <sanjoggaihre0@gmail.com>
2024-07-24 14:36:56 +0545

    created build directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  base: '/sanjog-portfolio/'
 })
